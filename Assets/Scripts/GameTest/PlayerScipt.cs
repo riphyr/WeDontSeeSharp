522905f6d11152d39cc83c7acc6a1d58ac7097de
riphyr <leo.deschanel-losciuto@epita.fr>
2024-11-20 21:11:23 +0100

    Creation of Pause Menu

--- Assets/Scripts/GameTest/PlayerScipt.cs
+++ Assets/Scripts/GameTest/PlayerScipt.cs
@@ -6,19 +6,25 @@ using Photon.Pun;
 
 public class PlayerScript : MonoBehaviour
 {
+	[Header("NUMERICAL PARAMETERS")]
 	public float walkSpeed = 5f;				// Vitesse de marche
 	public float runSpeed = 10f;				// Vitesse de course
     public float jumpForce = 4.5f;    			// Force de saut
     public float groundCheckRadius = 0.5f;  	// Rayon pour vérifier la présence du sol
 
+    [Header("LOOKING PARAMETERS")]
 	public float lookSensitivityX;    			// Vitesse de rotation de la caméra sur l'axe X
 	public float lookSensitivityY;    			// Vitesse de rotation de la caméra sur l'axe Y
     public float lookXLimit = 45f;  			// Limite de rotation verticale de la caméra
 	private float rotationX = 0;				// Initialisation de la rotation en X pour la caméra
 
+	[Header("ELEMENTS")]
 	public Camera playerCamera;					// GameObject relié à la caméra
 	public GameObject character;				// GameObject relié au prefab du joueur
 
+	[Header("MENUS")]
+	public GameObject pauseObject;				// Menu de pause
+	
     private Rigidbody rb;						// Composante de collision lié au moteur de jeu
 	private PhotonView view;					// Composante de Photon pour la différence entre local/server
 
@@ -38,6 +44,8 @@ public class PlayerScript : MonoBehaviour
 		// Blocage et disparition du curseur
 		Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        
+        pauseObject.SetActive(false);
     }
 
     void Update()
@@ -46,7 +54,7 @@ public class PlayerScript : MonoBehaviour
         if (view.IsMine)
         {
             // Vérification si le joueur est autorisé à bouger
-        	if (canMove)
+        	if (canMove && Cursor.lockState == CursorLockMode.Locked)
         	{
 				MovePlayer();																							// Fonction de déplacements latéraux
             	if (isGrounded) Jump();																					// Focntion de saut conditionné au sol
@@ -56,6 +64,28 @@ public class PlayerScript : MonoBehaviour
             	playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);								// Déplacement de la camera selon les mouvements de la souris
             	character.transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSensitivityY, 0);		// Rotation du joueur pour suivre les mouvements camera
         	}
+	        else
+	        {
+		        // Update des sensibilités de rotation camera
+		        lookSensitivityX = 2f * PlayerPrefs.GetFloat("sensitivityX", 2f);
+		        lookSensitivityY = 2f * PlayerPrefs.GetFloat("sensitivityY", 2f);
+	        }
+
+	        if (Input.GetKey(GetKeyCodeFromString(PlayerPrefs.GetString("Pause", "None"))))
+	        {
+		        pauseObject.SetActive(true);
+		        Cursor.lockState = CursorLockMode.None;
+		        Cursor.visible = true;
+	        }
+	        
+	        if (Cursor.lockState == CursorLockMode.None && Cursor.visible)
+	        {
+		        pauseObject.SetActive(true);
+	        }
+	        else
+	        {
+		        pauseObject.SetActive(false);
+	        }
         }
 
     }
--- WeDontSeeSharp/obj/Debug/net8.0/WeDontSeeSharp.AssemblyInfo.cs
+++ WeDontSeeSharp/obj/Debug/net8.0/WeDontSeeSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeDontSeeSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+341a55604100a21307552e2868d69ea930e69492")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b4d67d4b6a81041078c613a2610b4e70e45df0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeDontSeeSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeDontSeeSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
