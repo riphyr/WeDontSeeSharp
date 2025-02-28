f91ffbc34269aab5164b7a3a909c613493197c00
lae-titia <laetitia.berkoune@epita.fr>
2025-02-28 19:50:39 +0100

    Bon

--- Assets/Scripts/FirstPlace_Poker/Sitting_Multiplayer.cs
+++ Assets/Scripts/FirstPlace_Poker/Sitting_Multiplayer.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
-using Photon.Pun;
 
 namespace GameTest
 {
-    public class Sitting_Multiplayer : MonoBehaviourPunCallbacks
+    public class Sitting_SinglePlayer : MonoBehaviour
     {
         [Header("Player States")] 
         public GameObject playerStanding;
@@ -11,62 +10,49 @@ namespace GameTest
         public GameObject playerSitting1;
         public GameObject playerSitting2;
         public GameObject playerSitting3;
-        public GameObject CardsTable;
-        public GameObject Blackie;
+        //public GameObject CardsTable;
+        //public GameObject Blackie;
 
         [Header("UI Elements")] 
-        public GameObject intText; // Texte pour s'asseoir
+        public GameObject intText; 
 
         private bool interactable = false;
 
         private void OnTriggerEnter(Collider other)
         {
-            if (!other.CompareTag("Player") && !other.transform.root.CompareTag("Player")) return;
+            //if (!other.CompareTag("Player") && !other.transform.root.CompareTag("Player")) return;
 
-            PhotonView photonView = other.GetComponent<PhotonView>();
-
-            if (photonView != null && photonView.IsMine) 
-            {
-                intText.SetActive(true);
-                interactable = true;
-            }
+            intText.SetActive(true);
+            interactable = true;
         }
 
         private void OnTriggerExit(Collider other)
         {
-            if (!other.CompareTag("Player") && !other.transform.root.CompareTag("Player")) return;
-
-            PhotonView photonView = other.GetComponent<PhotonView>();
-
-            if (photonView != null && photonView.IsMine)
-            {
-                intText.SetActive(false);
-                interactable = false;
-            }
+            //if (!other.CompareTag("Player") && !other.transform.root.CompareTag("Player")) return;
+            intText.SetActive(false);
+            interactable = false;
         }
 
         private void Update()
         {
-            if (!photonView.IsMine) return; 
-
             if (interactable && Input.GetKeyDown(KeyCode.E) && !playerSitting.activeSelf)
             {
                 intText.SetActive(false);
-                photonView.RPC("Sit", RpcTarget.All); 
+                Sit();
             }
         }
 
-        [PunRPC] 
         private void Sit()
         {
-            playerSitting.SetActive(true);
             playerStanding.SetActive(false);
-
+            playerSitting.SetActive(true);
+            
+            /*
             if (playerSitting1.activeSelf && playerSitting2.activeSelf && playerSitting3.activeSelf)
             {
                 CardsTable.SetActive(true);
                 Blackie.SetActive(true);
-            }
+             */
         }
     }
 }
