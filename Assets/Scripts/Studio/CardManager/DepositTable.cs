216e131b099ecf8b0019e066e05286d3f98ced87
emma.rucay <emma.rucay@epita.fr>
2025-04-23 17:53:59 +0200

    fonction pick up cartes

---
+++ Assets/Scripts/Studio/CardManager/DepositTable.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class DepositTable : MonoBehaviourPunCallbacks
+{
+    //public Transform dropPoint; // Point où les cartes vont apparaître
+    public float spacing = 0.5f; // Espace entre les cartes
+    
+    [SerializeField] private Transform[] playerDropPoints; // assignés dans l'inspector
+
+    private Transform GetPlayerDropPoint()
+    {
+        int playerID = PhotonNetwork.LocalPlayer.ActorNumber;
+        int index = playerID % playerDropPoints.Length;
+        return playerDropPoints[index];
+    }
+
+
+    public void DepositCards()
+    {
+        int playerID = PhotonNetwork.LocalPlayer.ActorNumber;
+        var inventory = GameManager.instance.GetInventory(playerID);
+
+        if (inventory == null || inventory.Count == 0)
+        {
+            Debug.Log("Aucune carte à déposer.");
+            return;
+        }
+        Transform dropPoint = GetPlayerDropPoint();
+        Vector3 spawnPos = dropPoint.position;
+        //GameObject deckprefab = Resources.Load<GameObject>("DeckCard1");
+        //Vector3 spawnPos = dropPoint.position;
+        foreach (string cardName in inventory)
+        {
+            Debug.Log($"Card {cardName} deposer.");
+            GameObject prefab = GameManager.instance.GetCardPrefabByName(cardName);
+            if (prefab != null)
+            {
+                Debug.Log($"Bah c'est bien deposer {prefab.name}");
+                PhotonNetwork.Instantiate(prefab.name, spawnPos, Quaternion.identity);
+                spawnPos += new Vector3(spacing, 0, 0); // décaler à droite
+            }
+            else
+            {
+                Debug.Log($"Bah c'est nul :(");
+            }
+        }
+
+        // Vider l'inventaire du joueur après dépôt
+        GameManager.instance.ClearInventory(playerID);
+    }
+}
