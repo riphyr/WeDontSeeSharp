5c28d084c47cba97ec06ba2c196338db26158baa
doriane1881 <bouziane.doria@gmail.com>
2025-05-16 23:53:47 +0200

    CardScript

---
+++ Assets/Scripts/CardsScript/Card.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+// SCRIPT PRESENT SUR LES CARTES !!!!!
+
+namespace InteractionScripts
+{
+    [RequireComponent(typeof(PhotonView))]
+    public class Card : MonoBehaviourPunCallbacks
+    {
+        public string cardName; // Nom de la carte
+
+        void Start()
+        {
+            cardName = name;
+        }
+        public void Collect()
+        {
+            if (photonView == null)
+            {
+                return;
+            }
+
+            // Ajouter la carte à l’inventaire
+            if (PhotonNetwork.IsMasterClient)
+            {
+                photonView.RPC(nameof(DestroyCard), RpcTarget.AllBuffered);
+            }
+            else
+            {
+                photonView.RPC(nameof(RequestDestroyCard), RpcTarget.MasterClient);
+            }
+
+            CardManager.instance.CollectCard(cardName);
+        }
+
+
+
+        [PunRPC]
+        void RequestDestroyCard()
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                photonView.RPC(nameof(DestroyCard), RpcTarget.AllBuffered);
+            }
+        }
+
+        [PunRPC]
+        void DestroyCard()
+        {
+            gameObject.SetActive(false); 
+            Destroy(gameObject, 0.2f); 
+        }
+    }
+}
